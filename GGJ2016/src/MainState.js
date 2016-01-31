859e534a819f9c3ebe5d4c9ad475a3ec1c18143c
Carghaez <kejan73@gmail.com>
2016-01-31 16:31:22 +0100

    Commit sporco delle modifiche

--- GGJ2016/src/MainState.js
+++ GGJ2016/src/MainState.js
@@ -18,11 +18,69 @@ var ActorFlag = (function () {
     return ActorFlag;
 })();
 var Box = (function () {
-    function Box(sprite, gameState) {
-        this.sprite = sprite;
+    function Box(pos, gameState) {
         this.gameState = gameState;
+        this.gameState = gameState;
+        this.pos = new Phaser.Point(pos.x + 35, pos.y + 2);
+        this.spriteBlank = this.gameState.add.sprite(pos.x, pos.y, 'tile_blank');
+        this.spriteBlue = this.gameState.add.sprite(pos.x, pos.y, 'tile_blue');
+        this.spriteBlue.alpha = 0;
+        this.spriteRed = this.gameState.add.sprite(pos.x, pos.y, 'tile_red');
+        this.spriteRed.alpha = 0;
+        this.flag = ActorFlag.blank;
     }
+    Box.prototype.changeActor = function (index) {
+        if (index == 0) {
+            this.flag = ActorFlag.red;
+            this.spriteRed.alpha = 1;
+            this.spriteBlue.alpha = 0;
+        }
+    };
     return Box;
+})();
+var GridBoxes = (function () {
+    function GridBoxes(size, gameState) {
+        this.gameState = gameState;
+        this.initTiles = new Phaser.Point(128, 256);
+        this.initPlayers = new Array();
+        this.initPlayers.push(new Phaser.Point(163, 258));
+        this.boxes = [];
+        for (var i = 0; i < size.x; ++i) {
+            this.boxes.push(new Array());
+            for (var j = 0; j < size.y; ++j) {
+                this.boxes[i].push(new Box(new Phaser.Point(this.initTiles.x + (i * 64), this.initTiles.y + (j * (64 - 12))), gameState));
+            }
+        }
+    }
+    /*touch(playerPos: Phaser.Point, index: number): Phaser.Point {
+        var r = Math.ceil((playerPos.x - this.initPlayers[index].x) / 64);
+        var c = Math.ceil((playerPos.y - this.initPlayers[index].y) / 52);
+        let bp = this.boxes[r][c].pos;
+        let pp = playerPos;
+        if ((bp.x > playerPos.x - 5 && bp.x < playerPos.x + 5) && (bp.y > playerPos.y - 5 && bp.y < playerPos.y + 5)) {
+            this.boxes[r][c].changeActor(ActorFlag.red);
+            return bp;
+        }
+        return null;
+    }*/
+    GridBoxes.prototype.getNext = function (playerPos, dir, index) {
+        var r = Math.ceil((playerPos.y - this.initPlayers[index].y) / 52);
+        var c = Math.ceil((playerPos.x - this.initPlayers[index].x) / 64);
+        this.boxes[c][r].changeActor(index);
+        r += dir.y;
+        c += dir.x;
+        if (r < 0)
+            r = 0;
+        if (c < 0)
+            c = 0;
+        if (c >= this.boxes.length)
+            c = this.boxes.length - 1;
+        if (r >= this.boxes[c].length)
+            r = this.boxes[c].length - 1;
+        console.log("r:" + r + ", c:" + c);
+        return this.boxes[c][r].pos;
+    };
+    return GridBoxes;
 })();
 var ActorState = (function () {
     // boilerplate 
@@ -35,6 +93,7 @@ var ActorState = (function () {
     // values 
     ActorState.idle = new ActorState("idle");
     ActorState.walk = new ActorState("walk");
+    ActorState.canChangeDir = new ActorState("canChangeDir");
     ActorState.cast = new ActorState("cast");
     ActorState.die = new ActorState("die");
     ActorState.debug = new ActorState("debug");
@@ -87,6 +146,9 @@ var GameActor = (function () {
                 }
             }
         }
+        if (this.state === ActorState.debug) {
+            this.sprite.animations.stop();
+        }
         this.sprite.play(this.state.toString() + facing);
     };
     GameActor.prototype.setWalkDir = function (walkDir) {
@@ -95,21 +157,24 @@ var GameActor = (function () {
     GameActor.prototype.setNewDir = function (newDir) {
         this.newDir = newDir;
     };
-    GameActor.prototype.setSpeed = function (speed) {
+    /*setSpeed(speed: number):void {
         if (speed >= 0 && speed <= this.maxSpeed) {
             this.speed = speed;
         }
-    };
-    GameActor.prototype.footstep = function () {
+    }
+
+    footstep():void {
         if (this.state === ActorState.walk) {
             this.sprite.body.velocity.x = this.walkDir.x * this.speed;
             this.sprite.body.velocity.y = this.walkDir.y * this.speed;
+        } else {
+            this.sprite.body.velocity.x = this.sprite.body.velocity.y = 0;
         }
-    };
-    GameActor.prototype.walk = function () {
-        this.footstep();
+    }*/
+    /*walk(): void {
+        //this.footstep();
         this.playFrames();
-    };
+    }*/
     GameActor.prototype.update = function () {
     };
     return GameActor;
@@ -119,8 +184,11 @@ var Player = (function (_super) {
     function Player(sprite, gameState) {
         _super.call(this, sprite, gameState);
         this.gameState.physics.arcade.enable(this.sprite);
-        this.sprite.animations.add('idleDef', [0, 1, 2, 3], 4, true);
-        this.sprite.animations.add('walkRight', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 6, true);
+        this.sprite.animations.add('idleDef', [0, 1, 2, 3], 5, true);
+        this.sprite.animations.add('walkRight', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 10, true);
+        this.sprite.animations.add('walkLeft', [20, 21, 22, 23, 24, 25, 26, 27, 28, 29], 10, true);
+        this.sprite.animations.add('walkDown', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 10, true);
+        this.sprite.animations.add('walkUp', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49], 10, true);
         this.keys = this.gameState.input.keyboard.addKeys({
             'E': Phaser.Keyboard.E, 'Q': Phaser.Keyboard.Q, 'SPACEBAR': Phaser.Keyboard.SPACEBAR,
             'W': Phaser.Keyboard.W, 'A': Phaser.Keyboard.A, 'S': Phaser.Keyboard.S, 'D': Phaser.Keyboard.D,
@@ -131,20 +199,33 @@ var Player = (function (_super) {
     Player.prototype.getDirection = function () {
         // Check nuova direzione 
         var newDir = new Phaser.Point();
-        if (this.keys.W.isDown) {
+        if (this.keys.W.isDown || this.keys.up.isDown) {
             newDir.y = -1;
         }
-        else if (this.keys.S.isDown) {
+        else if (this.keys.S.isDown || this.keys.down.isDown) {
             newDir.y = +1;
         }
-        if (this.keys.A.isDown) {
-            newDir.x = -1;
-        }
-        else if (this.keys.D.isDown) {
-            newDir.x = +1;
-        }
+        if (newDir.y == 0)
+            if (this.keys.A.isDown || this.keys.left.isDown) {
+                newDir.x = -1;
+            }
+            else if (this.keys.D.isDown || this.keys.right.isDown) {
+                newDir.x = +1;
+            }
         return newDir;
     };
+    /*touchTiles(): boolean {
+        let pos: Phaser.Point;
+        pos = this.gameState.boxes.touch(new Phaser.Point(this.sprite.x, this.sprite.y), 0);
+        console.log(pos);
+        console.log("x:" + this.sprite.x+", y:"+this.sprite.y);
+        if (pos) {
+            this.sprite.x = pos.x;
+            this.sprite.y = pos.y;
+            return true;
+        }
+        return false;
+    }*/
     Player.prototype.update = function () {
         // Debug: Input check
         if (this.gameState.game.time.time > this.lastInputTime + 100 && (this.keys.E.isDown || this.keys.Q.isDown)) {
@@ -152,9 +233,10 @@ var Player = (function (_super) {
                 this.state = ActorState.debug;
                 this.sprite.animations.stop(null, false);
             }
+            var p;
             if (this.keys.E.isDown)
                 this.sprite.frame = +this.sprite.frame + 1;
-            else
+            if (this.keys.Q.isDown)
                 this.sprite.frame = +this.sprite.frame - 1;
             this.lastInputTime = this.gameState.game.time.time;
         }
@@ -170,16 +252,17 @@ var Player = (function (_super) {
             if (newDir.x !== 0 || newDir.y !== 0) {
                 this.setNewDir(newDir);
             }
-            // La nuova direzione dev'essere presa solo quando è in IDLE (cioè ha raggiunto una tile)
-            if (this.state === ActorState.idle) {
+            if (this.state === ActorState.canChangeDir) {
                 this.setWalkDir(this.newDir);
                 this.setState(ActorState.walk);
+                var p = this.gameState.boxes.getNext(new Phaser.Point(this.sprite.x, this.sprite.y), this.walkDir, 0);
+                this.gameState.add.tween(this.sprite).to(p, 500, "Linear", true).onComplete.add(this.touchTile, this);
             }
-            else {
-            }
-            if (this.state === ActorState.walk)
-                this.walk();
         }
+        this.playFrames();
+    };
+    Player.prototype.touchTile = function () {
+        this.setState(ActorState.canChangeDir);
     };
     return Player;
 })(GameActor);
@@ -210,16 +293,20 @@ var MainState = (function (_super) {
         this.load.spritesheet('player', 'assets/player_blue.png', 64, 64);
         this.load.spritesheet('tile_red', 'assets/tile_red.png', 64, 64);
         this.load.spritesheet('tile_blue', 'assets/tile_blue.png', 64, 64);
-        this.load.spritesheet('tile_base', 'assets/tile_blank.png', 64, 64);
+        this.load.spritesheet('tile_blank', 'assets/tile_blank.png', 64, 64);
     };
     MainState.prototype.create = function () {
         //this.game.stage.backgroundColor = '#509F00';
         this.add.sprite(0, 0, 'background');
-        this.player = new Player(this.add.sprite(150, 250, 'player'), this);
+        this.boxes = new GridBoxes(new Phaser.Point(16, 7), this);
+        this.player = new Player(this.add.sprite(163, 258, 'player'), this);
         this.frameText = this.add.text(20, 20, 'Player Frame: ' + this.player.sprite.frame, { fontSize: '16px', fill: '#000' });
     };
     MainState.prototype.play = function () {
-        this.player.setNewDir(new Phaser.Point(1, 0));
+        if (this.player.state === ActorState.idle) {
+            this.player.setNewDir(new Phaser.Point(1, 0));
+            this.player.setState(ActorState.canChangeDir);
+        }
     };
     MainState.prototype.update = function () {
         this.player.update();
--- GGJ2016/src/MainState.ts
+++ GGJ2016/src/MainState.ts
@@ -16,13 +16,83 @@
 class Box {
     pos: Phaser.Point;
     flag: ActorFlag;
-    sprite: Phaser.Sprite;
-    gameState: MainState;
+    spriteBlank: Phaser.Sprite;
+    spriteRed: Phaser.Sprite;
+    spriteBlue: Phaser.Sprite;
 
-    constructor(sprite: Phaser.Sprite, gameState: MainState) {
-        this.sprite = sprite;
+    constructor(pos: Phaser.Point, protected gameState: MainState) {
         this.gameState = gameState;
+        this.pos = new Phaser.Point(pos.x + 35, pos.y + 2);
+        this.spriteBlank = this.gameState.add.sprite(pos.x, pos.y, 'tile_blank');
+        this.spriteBlue = this.gameState.add.sprite(pos.x, pos.y, 'tile_blue');
+        this.spriteBlue.alpha = 0;
+        this.spriteRed = this.gameState.add.sprite(pos.x, pos.y, 'tile_red');
+        this.spriteRed.alpha = 0;
+        this.flag = ActorFlag.blank;
     }
+
+    changeActor(index: number): void {
+        if (index == 0) {
+            this.flag = ActorFlag.red;
+            this.spriteRed.alpha = 1;
+            this.spriteBlue.alpha = 0;
+        }
+    }
+}
+
+class GridBoxes {
+    boxes: Box[][];
+    initTiles: Phaser.Point;
+    initPlayers: Phaser.Point[];
+
+    constructor(size: Phaser.Point, protected gameState: MainState) {
+        this.initTiles = new Phaser.Point(128, 256);
+        this.initPlayers = new Array<Phaser.Point>();
+        this.initPlayers.push(new Phaser.Point(163, 258));
+        
+        this.boxes = [];
+        for (let i = 0; i < size.x; ++i) {
+            this.boxes.push(new Array<Box>());
+            for (let j = 0; j < size.y; ++j) {
+                this.boxes[i].push(new Box(new Phaser.Point(this.initTiles.x + (i * 64), this.initTiles.y + (j * (64 - 12))), gameState));
+            }
+        }
+    }
+
+    /*touch(playerPos: Phaser.Point, index: number): Phaser.Point {
+        var r = Math.ceil((playerPos.x - this.initPlayers[index].x) / 64);
+        var c = Math.ceil((playerPos.y - this.initPlayers[index].y) / 52);
+        let bp = this.boxes[r][c].pos;
+        let pp = playerPos;
+        if ((bp.x > playerPos.x - 5 && bp.x < playerPos.x + 5) && (bp.y > playerPos.y - 5 && bp.y < playerPos.y + 5)) {
+            this.boxes[r][c].changeActor(ActorFlag.red);
+            return bp;
+        }
+        return null;
+    }*/
+
+    getNext(playerPos: Phaser.Point, dir: Phaser.Point, index: number): Phaser.Point {
+        var r = Math.ceil((playerPos.y - this.initPlayers[index].y) / 52);
+        var c = Math.ceil((playerPos.x - this.initPlayers[index].x) / 64);
+
+        this.boxes[c][r].changeActor(index);
+
+        r += dir.y;
+        c += dir.x;
+
+        if (r < 0)
+            r = 0;
+        if (c < 0)
+            c = 0;
+        if (c >= this.boxes.length)
+            c = this.boxes.length - 1;
+        if (r >= this.boxes[c].length)
+            r = this.boxes[c].length - 1;
+        console.log("r:" + r + ", c:" + c);
+        return this.boxes[c][r].pos;
+    }
+
+
 }
 
 class ActorState {
@@ -37,6 +107,7 @@ class ActorState {
     // values 
     static idle = new ActorState("idle");
     static walk = new ActorState("walk");
+    static canChangeDir = new ActorState("canChangeDir");
     static cast = new ActorState("cast");
     static die = new ActorState("die");
     static debug = new ActorState("debug");
@@ -68,7 +139,7 @@ class GameActor {
         // L'attore cadrà a terrà e non potrà più muoversi
     }
 
-    setState(state: ActorState):void {
+    setState(state: ActorState): void {
         this.state = state;
     }
 
@@ -98,6 +169,9 @@ class GameActor {
                 }
             }
         }
+        if (this.state === ActorState.debug) {
+            this.sprite.animations.stop();
+        }
         this.sprite.play(this.state.toString()+facing);
     }
 
@@ -109,7 +183,7 @@ class GameActor {
         this.newDir = newDir;
     }
 
-    setSpeed(speed: number):void {
+    /*setSpeed(speed: number):void {
         if (speed >= 0 && speed <= this.maxSpeed) {
             this.speed = speed;
         }
@@ -119,13 +193,15 @@ class GameActor {
         if (this.state === ActorState.walk) {
             this.sprite.body.velocity.x = this.walkDir.x * this.speed;
             this.sprite.body.velocity.y = this.walkDir.y * this.speed;
+        } else {
+            this.sprite.body.velocity.x = this.sprite.body.velocity.y = 0;
         }
-    }
+    }*/
 
-    walk(): void {
-        this.footstep();
+    /*walk(): void {
+        //this.footstep();
         this.playFrames();
-    }
+    }*/
 
     update(): void {
 
@@ -141,8 +217,12 @@ class Player extends GameActor {
         super(sprite, gameState);
         this.gameState.physics.arcade.enable(this.sprite);
   
-        this.sprite.animations.add('idleDef', [0, 1, 2, 3], 4, true);
-        this.sprite.animations.add('walkRight', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 6, true);
+        this.sprite.animations.add('idleDef', [0, 1, 2, 3], 5, true);
+
+        this.sprite.animations.add('walkRight', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 10, true);
+        this.sprite.animations.add('walkLeft', [20, 21, 22, 23, 24, 25, 26, 27, 28, 29], 10, true);
+        this.sprite.animations.add('walkDown', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 10, true);
+        this.sprite.animations.add('walkUp', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49], 10, true);
 
         this.keys = this.gameState.input.keyboard.addKeys({
             'E': Phaser.Keyboard.E, 'Q': Phaser.Keyboard.Q, 'SPACEBAR': Phaser.Keyboard.SPACEBAR,
@@ -155,21 +235,34 @@ class Player extends GameActor {
     getDirection(): Phaser.Point {
         // Check nuova direzione 
         var newDir = new Phaser.Point();
-        if (this.keys.W.isDown) {
+        if (this.keys.W.isDown || this.keys.up.isDown) {
             newDir.y = -1;
         }
-        else if (this.keys.S.isDown) {
+        else if (this.keys.S.isDown || this.keys.down.isDown) {
             newDir.y = +1;
         }
-        if (this.keys.A.isDown) {
-            newDir.x = -1;
-        }
-        else if (this.keys.D.isDown) {
-            newDir.x = +1;
-        }
+        if (newDir.y == 0)
+            if (this.keys.A.isDown || this.keys.left.isDown) {
+                newDir.x = -1;
+            }
+            else if (this.keys.D.isDown || this.keys.right.isDown) {
+                newDir.x = +1;
+            }
         return newDir;
     }
 
+    /*touchTiles(): boolean {
+        let pos: Phaser.Point;
+        pos = this.gameState.boxes.touch(new Phaser.Point(this.sprite.x, this.sprite.y), 0);
+        console.log(pos);
+        console.log("x:" + this.sprite.x+", y:"+this.sprite.y);
+        if (pos) {
+            this.sprite.x = pos.x;
+            this.sprite.y = pos.y;
+            return true;
+        }
+        return false;
+    }*/
 
     update(): void {
         // Debug: Input check
@@ -179,10 +272,10 @@ class Player extends GameActor {
                 this.state = ActorState.debug;
                 this.sprite.animations.stop(null, false);
             }
-
+            let p;
             if (this.keys.E.isDown)
                 this.sprite.frame = +this.sprite.frame + 1;
-            else
+            if (this.keys.Q.isDown)
                 this.sprite.frame = +this.sprite.frame - 1;
             this.lastInputTime = this.gameState.game.time.time;
         }
@@ -191,10 +284,10 @@ class Player extends GameActor {
         if (!this.gameState.start && this.keys.SPACEBAR.isDown) {
             this.gameState.play();
         }
-    
+
+
         // Controllo se il personaggio è ancora vivo
         if (this.state !== ActorState.die && this.state !== ActorState.debug) {
-
             // Controllo se è stata premuta una direzione
             var newDir = this.getDirection();
             // Se ha scelto una nuova direzione la cambio
@@ -202,17 +295,18 @@ class Player extends GameActor {
                 this.setNewDir(newDir);
             }
 
-            // La nuova direzione dev'essere presa solo quando è in IDLE (cioè ha raggiunto una tile)
-            if (this.state === ActorState.idle) {
+            if (this.state === ActorState.canChangeDir) {
                 this.setWalkDir(this.newDir);
                 this.setState(ActorState.walk);
-            } else {
-                // dobbiamo metterlo in idle appena tocca la nuova tile
+                let p = this.gameState.boxes.getNext(new Phaser.Point(this.sprite.x, this.sprite.y), this.walkDir, 0);
+                this.gameState.add.tween(this.sprite).to(p, 500, "Linear", true).onComplete.add(this.touchTile, this);
             }
-
-            if (this.state === ActorState.walk)
-                this.walk();
         }
+        this.playFrames();
+    }
+
+    touchTile(): void {
+        this.setState(ActorState.canChangeDir);
     }
 }
 
@@ -220,6 +314,7 @@ class MainState extends Phaser.State {
     tiles: Phaser.Group;
     player: GameActor;
     start: boolean; 
+    boxes: GridBoxes;
     // Debug info
     frameText: Phaser.Text;
 
@@ -249,19 +344,24 @@ class MainState extends Phaser.State {
         this.load.spritesheet('player', 'assets/player_blue.png', 64, 64);
         this.load.spritesheet('tile_red', 'assets/tile_red.png', 64, 64);
         this.load.spritesheet('tile_blue', 'assets/tile_blue.png', 64, 64);
-        this.load.spritesheet('tile_base', 'assets/tile_blank.png', 64, 64);
+        this.load.spritesheet('tile_blank', 'assets/tile_blank.png', 64, 64);
 	}
 
 	create(): void {
         //this.game.stage.backgroundColor = '#509F00';
         this.add.sprite(0, 0, 'background');
 
-        this.player = new Player(this.add.sprite(150, 250, 'player'), this);
+        this.boxes = new GridBoxes(new Phaser.Point(16, 7), this);
+
+        this.player = new Player(this.add.sprite(163, 258, 'player'), this);
         this.frameText = this.add.text(20, 20, 'Player Frame: ' + this.player.sprite.frame, { fontSize: '16px', fill: '#000' });
     }
 
     play(): void {
-        this.player.setNewDir(new Phaser.Point(1, 0));
+        if (this.player.state === ActorState.idle) {
+            this.player.setNewDir(new Phaser.Point(1, 0));
+            this.player.setState(ActorState.canChangeDir);
+        }
     }
 
     update(): void {   
